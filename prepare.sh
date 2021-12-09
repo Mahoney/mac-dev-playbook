0ca60fbb935efa583193985aa6f5d1c6af2bf451
Robert Elliot <rob@lidalia.org.uk>
2021-12-09 23:34:50 +0000

    Reinstate installing ansible...

--- prepare.sh
+++ prepare.sh
@@ -14,11 +14,11 @@ main() {
     exit "$result"
   fi
 
-#  export PATH="$HOME/Library/Python/3.8/bin:$PATH"
-#
-#  sudo pip3 install --upgrade pip
-#  python3 -m pip install --user libyaml
-#  python3 -m pip install --user ansible
+  export PATH="$HOME/Library/Python/3.8/bin:$PATH"
+
+  sudo pip3 install --upgrade pip
+  python3 -m pip install --user libyaml
+  python3 -m pip install --user ansible
 }
 
 main "$@"
