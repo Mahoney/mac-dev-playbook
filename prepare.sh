ad4a18f1cbdd2b542f7b4c9180c806a681d55ce6
Robert Elliot <rob@lidalia.org.uk>
2021-12-09 23:32:05 +0000

    Handle xcode CL tools being installed

--- prepare.sh
+++ prepare.sh
@@ -5,13 +5,20 @@ IFS=$'\n\t'
 
 main() {
 
-  xcode-select --install
+  set +e
+  local install_output; install_output=$(xcode-select --install 2>&1)
+  local result=$?
+  set -e
+  if [[ "$result" -gt 1 || ("$result" == 1 && "$install_output" != *"already installed"* ) ]]; then
+    echo "$install_output" >&2
+    exit "$result"
+  fi
 
-  export PATH="$HOME/Library/Python/3.8/bin:$PATH"
-
-  sudo pip3 install --upgrade pip
-  python3 -m pip install --user libyaml
-  python3 -m pip install --user ansible
+#  export PATH="$HOME/Library/Python/3.8/bin:$PATH"
+#
+#  sudo pip3 install --upgrade pip
+#  python3 -m pip install --user libyaml
+#  python3 -m pip install --user ansible
 }
 
 main "$@"
